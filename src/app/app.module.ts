d5e6020f60d31785a345537d3deb7ae50bd2f0ba
VanCowboy <github@vancowboy.com>
2019-03-06 18:03:25 -0800

    Second Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EmpoloyeesComponent } from './empoloyees/empoloyees.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpoloyeesComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/empoloyees/empoloyees.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-empoloyees',
+  templateUrl: './empoloyees.component.html',
+  styleUrls: ['./empoloyees.component.css']
+})
+export class EmpoloyeesComponent implements OnInit {
+
+  employees: any;
+  offices: any;
+  constructor() {
+    this.employees = [
+      { fname: "John", lname: "James", state: "New York" },
+      { fname: "John", lname: "Booth", state: "Nebraska" },
+      { fname: "Steve", lname: "Smith", state: "Nebraska" },
+      { fname: "Stephanie", lname: "Smith", state: "New Hampshire" },
+      { fname: "Bill", lname: "Kydd", state: "New Mexico" },
+      { fname: "Bill", lname: "Cody", state: "Wyoming" }
+    ]
+
+    this.offices = [
+      { state: "New York", city: "Albany" },
+      { state: "Nebraska", city: "Omaha" },
+      { state: "New Mexico", city: "Albuquerque" },
+      { state: "New Hampshire", city: "Manchester" },
+      { state: "California", city: "Redding" }
+    ]
+
+    let finalArr = [];
+    let self = this;
+    for (let g = 0; g < self.employees.length; g++) {
+      for (let h = 0; h < self.offices.length; h++) {
+        if (self.employees[g]['state'] === self.offices[h]['state']) {
+          finalArr.push(self.employees[g]);
+          finalArr[finalArr.length - 1]['city'] = self.offices[h]['city'];
+          console.log(finalArr);
+          break;
+        }
+      }
+    }
+  }
+  ngOnInit() {
+  }
+
+}
